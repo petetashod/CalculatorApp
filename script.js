bcfd539dddd1c98f0c9bee59584ffef434f64b65
tashod peterson <tashodpeterson@tashods-MacBook-Air.local>
2023-05-30 18:35:33 -0400

    first

--- script.js
+++ script.js
@@ -1,62 +1,77 @@
+/** @format */
+
 let display = document.getElementById("display");
-const numbers = document.querySelectorAll('.numbers');
-const operations = document.querySelector('.operations')
-const equals = document.getElementById('equals');
-const clearButton = document.getElementById('clear');
-let result;
-let operator;
-let numberOne;
-let numberTwo;
+const numbers = document.querySelectorAll(".numbers");
+let operations = document.querySelectorAll(".operations");
+const equals = document.getElementById("equal");
+const clearButton = document.getElementById("clearButton");
+let result = "";
+let operator = "";
+let firstOperand = "";
+let secondOperand = "";
 
 numbers.forEach((number) => {
-    number.addEventListener('click',(e)=>{
-        let number = e.target.textContent
-        screenDisplay();
-        //console.log(number)
-        ///parseInt(number)
-    })
-})
-console.log(numbers)
-Array.from(operations).forEach((operation) => {
-    operation.addEventListener('click',(e) =>{
-        operations = e.target.textContent
-    })
-})
-
-clearButton.addEventListener('click',clear);
+  number.addEventListener("click", (e) => {
+    firstOperand = e.target.textContent;
+    screenDisplay();
+    //console.log(numbers)
+    ///parseInt(number)
+  });
+});
 
 
-function clear(){
-    display.textContent = '';
-    let numberONe= "";
-    let result = "";
-    let operator = "";
-    let numberTwo = "";
+operations.forEach((operation) => {
+  operation.addEventListener("click", (e) => {
+    operations = e.target.textContent;
+    console.log(operations)
+    // if (firstOperand && secondOperand){
+    //     calculations();
+    // }
+  });
+});
 
-};
+clearButton.addEventListener("click", clear);
+equals.addEventListener("click", calculations);
 
-function screenDisplay(){
-    let display = numbers.value;
+function clear() {
+  display.textContent = "";
+  firstOperand = "";
+  result = "";
+  operator = "";
+  secondOperand = "";
 }
 
+function screenDisplay() {
+    display.textContent = firstOperand;
+  if (firstOperand !== null) {
+    firstOperand = secondOperand;
+    firstOperand = "";
+  }  
+  if (firstOperand && secondOperand && operator){
+    display.textContent = result;
 
-function calculations(){
-    let numberOne = parseFloat(firstSelection)
-    let numberTwo = parseFloat(secondSelection)
-    if (operator == '+'){
-        result = numberOne + numberTwo
-        return parseFloat(result.toFixed(3))
-    }
+   }
 
-    else if (operator == '-'){
-        return parseFloat(result.toFixed(3))
-    }
-    else if(operator == 'X'){
-        return parseFloat(result.toFixed(3))
+}
 
-    }
-    else if (operator == '/'){
-        return parseFloat(result.toFixed(3))
+function calculations() {
+  let numberOne = parseFloat(firstOperand);
+  let numberTwo = parseFloat (secondOperand);
+  if (operator == "+") {
+    result=numberOne + numberTwo;
+    parseFloat(result.toFixed(3));
+    firstNumber = "";
 
-    }
+    return (display.textContent = result);
+  } else if (operator == "-") {
+    result= numberOne - numberTwo;
+    return parseFloat(result.toFixed(3));
+  } else if (operator == "X") {
+    result= numberOne * numberTwo;
+    return parseFloat(result.toFixed(3));
+  } else if (operator == "/") {
+    result= numberOne / numberTwo;
+    return parseFloat(result.toFixed(3));
+  }
+  
 }
